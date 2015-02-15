ef4f93d3ea6d233140bf38909e064469d6d49e69
Emily Pakulski <enp2111@columbia.edu>
2015-02-14 23:29:07 -0500

    Switched text extraction tools from Readability API to Unfluff Node module.

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,43 @@
-
+var extractor = require('unfluff');
 var router = require('express').Router();
+var request = require('request');
+
+var makeRequest = function(articleUrl, callback) {
+	var options = {
+		url: articleUrl,
+		method: 'GET',
+		json: true
+	}
+	
+	var req = request(options, function(err, response, body) {
+		if (err) {
+			callback(err);
+		} else {
+			callback(null, body);
+		}
+	})
+	
+	return req;
+}	
 
 router.get('/extract',function(req, res){
     var articleUrl = req.query.url;
 
-    if (!articleUrl)
+    if (!articleUrl) 
         return res.status(400).json({error:' url not found'});
     else {
-        req.readability.getText(articleUrl, function(err, text) {
-            req.relationship_extraction.extract({text:text, dataset:'ie-en-news'}, function (err, result){
+        makeRequest(articleUrl, function(err, text) {
+			var justText = extractor(text, 'en');
+		
+            req.relationship_extraction.extract({text:justText['text'], dataset:'ie-en-news'}, function (err, result){
                 if (err)
                     res.json(err);
                 else {
-                    res.json({xml:result});
+					// text -- plaintext
+					// parse -- watsonified version
+                    res.json({
+						'watson' : result
+					});
                 }
             });
         })
