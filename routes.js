bf3b9343b4e57408fcdad517bba3392fe7875133
Amarto <amarto.rajaram@gmail.com>
2015-02-14 19:27:22 -0500

    Started adding in Watson support for Node

--- routes.js
+++ routes.js
@@ -1,13 +1,19 @@
-/* This file is required by app.js. 
+/* This file is required by app.js.
  * It sets up the application endpoints/routes.
  */
 
-module.exports = function(app){
+module.exports = function(app, watson){
 	app.post('/api/url_readability', function(req, res) {
+		// var articleText = 'Tigers hate Barack Obama';
+		// watson.extract(articleText, )
+
 		res.send({content : 'check'});
+
 	});
-	
+
 	app.get('/*', function(req, res){
 		res.render('home.ejs');
 	});
+
+
 };
